50e2adfb3bfbfde1b5d11c51477349730c7db830
SightStudio <sdm3285@gmail.com>
2019-08-31 20:45:19 +0900

    [2019.08.31] 비즈니스 로직 exception에 HTTP 응답코드 필드 추가

--- src/java/com/common/enumtype/BizExceptionCode.java
+++ src/java/com/common/enumtype/BizExceptionCode.java
@@ -13,22 +13,22 @@ import java.util.Arrays;
 public enum BizExceptionCode {
 	
 	// 정상 에러코드 
-	COMMON_ERROR_000000("000000", "정상"),
+	COMMON_ERROR_000000(200, "000000", "정상"),
 	
 	// 비정상 에러코드 (공통)
-	COMMON_ERROR_000001("000001", "공통 [unknown exception] 알 수 없는 에러입니다."),
+	COMMON_ERROR_000001(500,"000001", "공통 [unknown exception] 알 수 없는 에러입니다."),
 	
 	// 유저 관련 
 	//   [1] JWT 인증 관련
-	COMMON_ERROR_100001("100001", "로그인 시간이 만료되었습니다"),
-	COMMON_ERROR_100002("100002", "로그인 [unknown exception] 관리자에게 문의해주세요");
+	COMMON_ERROR_100001(412,"100001", "로그인 시간이 만료되었습니다"),
+	COMMON_ERROR_100002(403,"100002", "로그인 [unknown exception] 관리자에게 문의해주세요");
 	
+	private int    replStatus;	// http 응답코드
+	private String errMsg;		// 에러 메시지
+	private String errCode;		// 에러 코드
 	
-	
-	private String errMsg;
-	private String errCode;
-	
-	private BizExceptionCode(String errCode, String errMsg) {
+	private BizExceptionCode(int repl_status ,String errCode, String errMsg) {
+		this.replStatus = repl_status;
 		this.errCode = errCode;
 		this.errMsg = errMsg;
 	}
@@ -41,6 +41,10 @@ public enum BizExceptionCode {
 		return errCode;
 	}
 	
+	public int getReplStatus() {
+		return replStatus;
+	}
+
 	/**
 	 * <pre>
 	 * 에러코드 기반 에러 조회 enum method
